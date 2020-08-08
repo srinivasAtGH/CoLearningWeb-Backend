148df3d7e71b7a9dddcf728646542b9b1839c2c2
vipulss <vipulss@gmail.com>
2020-08-08 17:54:13 +0530

    Updated swagger.yaml path in code (related to the earlier commit)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var router = require("express").Router();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./postman/swagger.yaml');
+const swaggerDocument = YAML.load('./Docs/swagger.yaml');
   
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
